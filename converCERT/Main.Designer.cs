fc2246e1ea7ffc2e6a009f56700b24366955c6b3
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-02 12:01:21 +0700

    update

---
+++ converCERT/Main.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace converCERT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.certTxt = new System.Windows.Forms.TextBox();
+            this.keyTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.certBtn = new System.Windows.Forms.Button();
+            this.keyBtn = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // certTxt
+            // 
+            this.certTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.certTxt.Location = new System.Drawing.Point(70, 15);
+            this.certTxt.Name = "certTxt";
+            this.certTxt.Size = new System.Drawing.Size(351, 24);
+            this.certTxt.TabIndex = 0;
+            // 
+            // keyTxt
+            // 
+            this.keyTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyTxt.Location = new System.Drawing.Point(70, 48);
+            this.keyTxt.Name = "keyTxt";
+            this.keyTxt.Size = new System.Drawing.Size(351, 24);
+            this.keyTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CERT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "KEY";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertBtn.Location = new System.Drawing.Point(508, 15);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(96, 57);
+            this.convertBtn.TabIndex = 4;
+            this.convertBtn.Text = "CONVERT";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // certBtn
+            // 
+            this.certBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.certBtn.Location = new System.Drawing.Point(427, 15);
+            this.certBtn.Name = "certBtn";
+            this.certBtn.Size = new System.Drawing.Size(75, 24);
+            this.certBtn.TabIndex = 5;
+            this.certBtn.Text = "SELECT";
+            this.certBtn.UseVisualStyleBackColor = true;
+            this.certBtn.Click += new System.EventHandler(this.certBtn_Click);
+            // 
+            // keyBtn
+            // 
+            this.keyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyBtn.Location = new System.Drawing.Point(427, 48);
+            this.keyBtn.Name = "keyBtn";
+            this.keyBtn.Size = new System.Drawing.Size(75, 24);
+            this.keyBtn.TabIndex = 6;
+            this.keyBtn.Text = "SELECT";
+            this.keyBtn.UseVisualStyleBackColor = true;
+            this.keyBtn.Click += new System.EventHandler(this.keyBtn_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 95);
+            this.Controls.Add(this.keyBtn);
+            this.Controls.Add(this.certBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyTxt);
+            this.Controls.Add(this.certTxt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "ConvertCERT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox certTxt;
+        private System.Windows.Forms.TextBox keyTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button certBtn;
+        private System.Windows.Forms.Button keyBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ converCERT/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace converCERT
+{
+    public partial class Main : Form
+    {
+        //private string cmd = "openssl pkcs12 -export -out domain.name.pfx -inkey domain.name.key -in domain.name.crt";
+        private string cmd = "openssl pkcs12 -export -out {0} -inkey {1} -in {2}";
+        private string _cert;
+        string _key;
+        Process process;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            var outFile = $"{Path.GetFileNameWithoutExtension(_key)}.pfx";
+
+            process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "cmd";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+
+            process.StandardInput.Write(string.Format(cmd, _key, _cert));
+        }
+
+        private void certBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _cert = openFileDialog.FileName;
+                certTxt.Text = _cert;
+            }
+        }
+
+        private void keyBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _key = openFileDialog.FileName;
+                keyTxt.Text = _key;
+            }
+        }
+    }
+}
