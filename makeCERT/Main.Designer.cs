f167055acc7c604e1bfce3a5eaeda71880b6b11e
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-02 10:43:41 +0700

    update

---
+++ makeCERT/Main.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace makeCERT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genCertBtn = new System.Windows.Forms.Button();
+            this.hostNameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.genCertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genCertBtn.Location = new System.Drawing.Point(333, 15);
+            this.genCertBtn.Name = "button1";
+            this.genCertBtn.Size = new System.Drawing.Size(89, 56);
+            this.genCertBtn.TabIndex = 0;
+            this.genCertBtn.Text = "Gen CERT";
+            this.genCertBtn.UseVisualStyleBackColor = true;
+            this.genCertBtn.Click += new System.EventHandler(this.genCertBtn_Click);
+            // 
+            // hostNameTextBox
+            // 
+            this.hostNameTextBox.Location = new System.Drawing.Point(15, 87);
+            this.hostNameTextBox.Multiline = true;
+            this.hostNameTextBox.Name = "hostNameTextBox";
+            this.hostNameTextBox.Size = new System.Drawing.Size(407, 192);
+            this.hostNameTextBox.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(105, 15);
+            this.emailTextBox.Name = "textBox2";
+            this.emailTextBox.Size = new System.Drawing.Size(212, 23);
+            this.emailTextBox.TabIndex = 2;
+            this.emailTextBox.Text = "example";
+            this.emailTextBox.TextChanged += new System.EventHandler(this.hostNameTxt_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(105, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 23);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "webmaster@example.com";
+            this.textBox3.TextChanged += new System.EventHandler(this.emailTxt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "HOST NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "EMAIL";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 319);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.hostNameTextBox);
+            this.Controls.Add(this.genCertBtn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "makeCERT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genCertBtn;
+        private System.Windows.Forms.TextBox hostNameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ makeCERT/Main.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace makeCERT
+{
+    public partial class Main : Form
+    {
+        private string _hostName = "example";
+        private string _email = "webmaster@example.com";
+        private string batText = @"
+@echo off
+
+REM IN YOUR SSL FOLDER, SAVE THIS FILE AS: makeCERT.bat
+REM AT COMMAND LINE IN YOUR SSL FOLDER, RUN: makecert
+REM IT WILL CREATE THESE FILES: example.cnf, example.crt, example.key
+REM IMPORT THE .crt FILE INTO CHROME Trusted Root Certification Authorities
+REM REMEMBER TO RESTART APACHE OR NGINX AFTER YOU CONFIGURE FOR THESE FILES
+
+REM PLEASE UPDATE THE FOLLOWING VARIABLES FOR YOUR NEEDS.
+SET HOSTNAME={0}
+SET DOT=com
+SET COUNTRY=US
+SET STATE=KS
+SET CITY=Olathe
+SET ORGANIZATION=IT
+SET ORGANIZATION_UNIT=IT Department
+SET EMAIL={1}
+
+(
+echo [req]
+echo default_bits = 2048
+echo prompt = no
+echo default_md = sha256
+echo x509_extensions = v3_req
+echo distinguished_name = dn
+echo:
+echo [dn]
+echo C = %COUNTRY%
+echo ST = %STATE%
+echo L = %CITY%
+echo O = %ORGANIZATION%
+echo OU = %ORGANIZATION_UNIT%
+echo emailAddress = %EMAIL%
+echo CN = %HOSTNAME%.%DOT%
+echo:
+echo [v3_req]
+echo subjectAltName = @alt_names
+echo:
+echo [alt_names]
+echo DNS.1 = *.%HOSTNAME%.%DOT%
+echo DNS.2 = %HOSTNAME%.%DOT%
+)>%HOSTNAME%.cnf
+
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout %HOSTNAME%.key -days 3560 -out %HOSTNAME%.crt -config %HOSTNAME%.cnf
+";
+        public Main()
+        {
+            InitializeComponent();
+            hostNameTextBox.Text = _hostName;
+            emailTextBox.Text = _email;
+        }
+
+        Process process;
+        private void genCertBtn_Click(object sender, EventArgs e)
+        {
+            process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "cmd";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.Start();
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerAsync();
+
+            process.StandardInput.Write(string.Format(batText, _hostName, _email));
+        }
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string line;
+            while (!process.StandardOutput.EndOfStream)
+            {
+                line = process.StandardOutput.ReadLine();
+                if (!string.IsNullOrEmpty(line))
+                {
+                    SetText(line);
+                }
+            }
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.hostNameTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.hostNameTextBox.Text += text + Environment.NewLine;
+            }
+        }
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            process.StandardInput.WriteLine("exit");
+            process.Close();
+
+        }
+
+        private void hostNameTxt_TextChanged(object sender, EventArgs e)
+        {
+            _hostName = hostNameTextBox.Text;
+        }
+
+        private void emailTxt_TextChanged(object sender, EventArgs e)
+        {
+            _email = emailTextBox.Text;
+        }
+    }
+}
