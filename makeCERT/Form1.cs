c0a2d71a37db8b5f2d9459dde82388d055d73971
dan nguyen <ddnguyen0702@hotmail.com>
2018-04-02 10:12:14 +0700

    update

--- makeCERT/Form1.cs
+++ makeCERT/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,22 @@ namespace makeCERT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("batchfile1.bat")
+            {
+                UseShellExecute = false
+            };
+
+            Process batchProcess = new Process
+            {
+                StartInfo = processInfo
+            };
+
+            batchProcess.Start();
+
         }
     }
 }
